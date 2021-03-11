e2eee4d012da7a4652d8afbc6169b2a6b6be8ba5
POYI LEE <s5819874@hotmail.com>
2021-03-11 15:56:50 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -50,4 +50,9 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect: '/users/login'
 }))
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 module.exports = router
