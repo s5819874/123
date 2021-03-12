b996b04e604507e708abdfc7fdd55b15548c6d0f
POYI LEE <s5819874@hotmail.com>
2021-03-12 09:21:29 +0800

    feat: add isAuthenticated to show different templates

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
 //start and listen on the server
