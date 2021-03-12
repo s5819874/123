25c8acc8059a94fd4b99673c7238a94996950974
Po <s5819874@hotmail.com>
2021-03-12 22:19:08 +0800

    feat: add dotenv in mongoose config to fix MONGODB_URI reading problem

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 //set database connection
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 //get status of connection of database
 const db = mongoose.connection
 db.on('error', () => {
